9481540881f0728636e8038cb7221ab1bae660b4
Timur Krasnopir <timur.krasnopir@unicyb.kiev.ua>
2020-09-17 19:11:34 +0300

    Implement the fixed-point iteration method

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -1,9 +1,40 @@
 #include <iostream>
+#include <cmath>
+#include <cstddef>
 
-using namespace std;
+double phi(double x) {
+    return (1.0 - x * x * x * x) / 2.0; // (1 - x^4) / 2
+}
+
+void fixedPointIteration(double initialValue, double eps) {
+    std::cout << "n = 0: " << std::endl;
+    std::cout << "\tx_{0} = " << initialValue << std::endl;
+
+    double previousValue = initialValue; // x_{n-1}
+    double currentValue = phi(initialValue); // x_{n}
+    std::size_t n = 1;
+
+    std::cout << "n = " << n << ": " << std::endl;
+    std::cout << "\tx_{" << n << "} = " << currentValue << std::endl;
+    std::cout << "\tx_{" << n - 1 << "} - x_{" << n << "} = " << previousValue - currentValue << std::endl;
+
+    while(std::fabs(currentValue - previousValue) >= eps) {
+        previousValue = currentValue;
+        currentValue = phi(previousValue);
+        n++;
+
+        std::cout << "n = " << n << ": " << std::endl;
+        std::cout << "\tx_{" << n << "} = " << currentValue << std::endl;
+        std::cout << "\tx_{" << n - 1 << "} - x_{" << n << "} = " << previousValue - currentValue << std::endl;
+    }
+}
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    const double eps = 0.0000001;
+    const double initialValue = 0.5;
+
+    std::cout << "Fixed-point iteration: " << std::endl;
+    fixedPointIteration(initialValue, eps);
     return 0;
 }
