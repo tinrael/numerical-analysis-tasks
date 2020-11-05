e9854573093c39af50291ebc538093a95adb0bdb
Timur Krasnopir <timur.krasnopir@unicyb.kiev.ua>
2020-11-05 22:08:55 +0200

    Implement the printMatrix function

--- lab2/main.cpp
+++ lab2/main.cpp
@@ -1,9 +1,30 @@
 #include <iostream>
+#include <cstddef>
 
-using namespace std;
+const std::size_t columns = 5;
+const std::size_t rows = 4;
+
+void printMatrix(double a[][columns]) {
+    for (std::size_t i = 0; i < rows; i++) {
+        for (std::size_t j = 0; j < columns; j++) {
+            std::cout << a[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
+}
 
 int main()
 {
-    cout << "Hello world!" << endl;
+
+
+    double a[rows][columns] = {
+        {3.0, 1.0, -1.0, 2.0, 6.0},
+        {-5.0, 1.0, 3.0, -4.0, -12.0},
+        {2.0, 0.0, 1.0, -1.0, 1.0},
+        {1.0, -5.0, 3.0, -3.0, 3.0}
+    };
+
+    printMatrix(a);
+
     return 0;
 }
