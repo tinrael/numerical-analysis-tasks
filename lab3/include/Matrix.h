3a962231d4f0842d87ea7d27679f4a8c55d22268
Timur Krasnopir <timur.krasnopir@unicyb.kiev.ua>
2020-12-04 04:58:05 +0200

    Implement some basic methods

--- lab3/include/Matrix.h
+++ lab3/include/Matrix.h
@@ -1,16 +1,24 @@
 #ifndef MATRIX_H
 #define MATRIX_H
 
+#include <cstddef>
 
 class Matrix
 {
     public:
-        Matrix();
+        Matrix(std::size_t rows, std::size_t columns);
         virtual ~Matrix();
+
+        double& operator()(std::size_t i, std::size_t j);
+
+        void print();
 
     protected:
 
     private:
+        std::size_t rows;
+        std::size_t columns;
+        double** a;
 };
 
 #endif // MATRIX_H
--- lab3/src/Matrix.cpp
+++ lab3/src/Matrix.cpp
@@ -1,11 +1,46 @@
 #include "Matrix.h"
 
-Matrix::Matrix()
+#include <iostream>
+
+Matrix::Matrix(std::size_t rows, std::size_t columns)
 {
-    //ctor
+    this->rows = rows;
+    this->columns = columns;
+
+    a = new double*[rows];
+    for (std::size_t i = 0; i < rows; i++) {
+        a[i] = new double[columns];
+    }
+
+    for (std::size_t i = 0; i < rows; i++) {
+        for (std::size_t j = 0; j < columns; j++) {
+            a[i][j] = 0.0;
+        }
+    }
 }
 
 Matrix::~Matrix()
 {
-    //dtor
+    for (std::size_t i = 0; i < rows; i++) {
+        delete[] a[i];
+    }
+
+    delete[] a;
+}
+
+double& Matrix::operator()(std::size_t i, std::size_t j) {
+    if (i >= this->rows || j >= this->columns) {
+        throw std::invalid_argument( "out-of-bounds" );
+    }
+
+    return a[i][j];
+}
+
+void Matrix::print() {
+    for (std::size_t i = 0; i < rows; i++) {
+        for (std::size_t j = 0; j < columns; j++) {
+            std::cout << a[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
 }
